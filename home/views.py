318c2ec87d60af127a24ab572a297c09efd2ce7f
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-05 21:20:28 +0800

    created booklistview

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from .models import Book
+from .serializers import BookSerializer
+
+from rest_framework import mixins
+from rest_framework import generics
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'home/home.html')
+class BookList(mixins.ListModelMixin, generics.GenericAPIView):
+    
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
