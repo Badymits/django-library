e68a56e33d40f87a944d49d1a2da1343691107ca
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-05 18:20:23 +0800

    configured path for uploaded images

--- personal/settings.py
+++ personal/settings.py
@@ -179,6 +179,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# all images will be stored in this path
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
