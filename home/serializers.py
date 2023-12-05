0e567fe2be660b56e65ebf2fa34ce833ad302fae
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-05 23:03:44 +0800

    added author and genre serializer to serialize foreign key fields

--- home/serializers.py
+++ home/serializers.py
@@ -4,9 +4,22 @@ from .models import *
 
  # just in case Serialization = The process whereby an object or data structure 
  # is translated into a format suitable for transfer over a network, or storage (e.g. in an array buffer or file format)
- 
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name']
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name']
 
 class BookSerializer(serializers.ModelSerializer):
+    
+    # this fixes the problem where it only shows the foreignkey ID, not the returned name string
+    author          = AuthorSerializer(read_only=True)
+    genre           = GenreSerializer(read_only=True, many=True)
     
     class Meta:
         model = Book
