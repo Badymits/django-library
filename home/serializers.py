8ee535a900fb66df00f7604becc455f71a15569e
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-05 21:19:30 +0800

    added spacing between comments

--- home/serializers.py
+++ home/serializers.py
@@ -4,6 +4,8 @@ from .models import *
 
  # just in case Serialization = The process whereby an object or data structure 
  # is translated into a format suitable for transfer over a network, or storage (e.g. in an array buffer or file format)
+ 
+
 class BookSerializer(serializers.ModelSerializer):
     
     class Meta:
