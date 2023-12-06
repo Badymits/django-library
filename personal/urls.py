e0a6a9c209ea7d3f4e79d6f4078fcc7e2f9e54b0
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-07 01:09:32 +0800

    added url route for static and media files

--- personal/urls.py
+++ personal/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from accounts.views import MyTokenObtainPairView
 
 from rest_framework_simplejwt.views import (
@@ -31,3 +34,5 @@ urlpatterns = [
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
