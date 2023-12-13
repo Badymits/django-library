9617c3ee8d51ec712812e1b77bdcaa6e8aadc3f3
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-14 00:38:20 +0800

    added endpoint for get book detail

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('get-book-list/', views.BookListView.as_view(), name='get-book-list'),
+    path('get-book-detail/<int:pk>/', views.BookDetailView.as_view(), name='get-book-detail')
 ]
