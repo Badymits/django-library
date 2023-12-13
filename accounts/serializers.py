df0105dce421fa765c1b6b966f7af0a0391f140f
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-14 00:37:43 +0800

    updated comments

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -54,7 +54,7 @@ class AccountRegisterSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
     
-    # this is only to set the password for the user since the set password in the model does not work
+    # this is only to set the password for the user since the set password in the model.py does not work
     def create(self, validated_data):
         password = validated_data.pop('password', None)
         instance = self.Meta.model(**validated_data)
