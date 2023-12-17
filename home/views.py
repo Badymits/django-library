1afc71d779a728dbdf69d280497bd64da636db9d
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-17 19:01:57 +0800

    improved search filtering in booksearchview, added comments

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from .serializers import BookSerializer
 from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.response import Response
+from django.db.models import Q
 
 
 # Create your views here.
@@ -22,19 +23,19 @@ class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookSerializer
     
     def get(self, request, *args, **kwargs):
-        print(request)
+        #print(request)
         return self.retrieve(request, *args, **kwargs)
     
 # create class that would get books based on genre
-class BookGenreListView(mixins.ListModelMixin, generics.GenericAPIView):
+class BookSearchListView(mixins.ListModelMixin, generics.GenericAPIView):
     
     queryset = Book.objects.all()
-    serializer_class = BookSerializer(queryset, many=True)  
+    serializer_class = BookSerializer(queryset, many=True) 
     
     def get(self, request, *args, **kwargs):
         
         queryset = self.get_queryset()
-        
+
         # the query set needs to be serialized first so that it can be sent  to the frontend
         serializer_class = BookSerializer(queryset, many=True)  
         
@@ -44,15 +45,19 @@ class BookGenreListView(mixins.ListModelMixin, generics.GenericAPIView):
         
     # this method is called inside of get, we just need to override it
     def get_queryset(self):
-        print('Hello')
-        queryset = Book.objects.all()
-        genre_filter = self.request.query_params.get('genre')
         
-        # verify first if not empty
-        if genre_filter is not None:
-            return Book.objects.filter(genre__name=genre_filter)
+        # retrieve url parameter
+        query_param = self.request.query_params.get('key')
         
-        return queryset
+        # use the url paramter to filter books based on url param
+        qs_filtered = Book.objects.filter(
+            Q(title__icontains=query_param) |
+            Q(author__name__icontains=query_param) |
+            Q(genre__name__icontains=query_param)
+        )
+    
+        
+        return qs_filtered
     
         
     
