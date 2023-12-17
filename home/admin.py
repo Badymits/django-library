6f765139f8e70f9af221cd5d5bb64be713734306
Elijah Cajucom <elijah.cajucom@gmail.com>
2023-12-17 19:02:19 +0800

    added search fields and list display cols

--- home/admin.py
+++ home/admin.py
@@ -4,8 +4,16 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title', 'genre__name', 'author__name',)
+    
+class AuthorAdmin(admin.ModelAdmin):
+    
+    search_fields = ('books__title',)
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
 admin.site.register(CheckedOutBooks)
 
