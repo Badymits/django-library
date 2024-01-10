677e5017aa611d94b64f173ea055e0b4afbd33fc
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-01-10 23:00:54 +0800

    added url path for create payment intent view

--- payments/urls.py
+++ payments/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('test-payment/', views.test_payment, name='test-payment'),
+    path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
 ]
 
