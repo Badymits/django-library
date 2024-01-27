921ff6a316fb2dcf4a05b6b904104cd5918e791a
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-01-27 23:21:16 +0800

    added get pub key url path

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('test-payment/', views.test_payment, name='test-payment'),
+    path('get-pub-key/', views.publishable_key, name='get-pub-key'),
     path('create-payment-intent/', views.create_payment_intent, name='create-payment-intent'),
+    path('checkout-session/', views.create_checkout_session, name='checkout-session'),
 ]
 
