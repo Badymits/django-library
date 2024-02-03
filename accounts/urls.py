8d1d582251844d22720a9f5f4bcd0ec27d81b51c
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-04 00:07:25 +0800

    added url path for user profile and user edit profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
-    
-    path('user-list/', views.userList, name='user-list')
+    path('user-profile/<str:username>/', views.AccountDetailView.as_view(), name='user-profile'),
+    path('user-list/', views.userList, name='user-list'),
+    path('user-edit-profile/<str:username>/', views.EditAccountView.as_view(), name='user-edit-profile'),
 ]
 
 
